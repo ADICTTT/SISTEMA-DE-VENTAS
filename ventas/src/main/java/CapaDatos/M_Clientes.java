c166a671663afd0162c57012a28563b241fae068
ADICT <choquecallatadriana@gmail.com>
2024-08-17 23:11:57 -0400

    creacion de la clase N_Cliente, M_Clientes, iniciamos con el fromCliente (entorno visual) y creamos en sql procedimientos almacenados

---
+++ ventas/src/main/java/CapaDatos/M_Clientes.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaDatos;
+
+/**
+ *
+ * @author ADRIANA
+ */
+public class M_Clientes {
+    private String codigo;
+    private String apellidos;
+    private String nombre;
+    private String ci;
+    private int edad;
+    private String sexo;
+    private String telefono;
+    private String direccion;
+
+    public M_Clientes(String codigo, String apellidos, String nombre, String ci, int edad, String sexo, String telefono, String direccion) {
+        this.codigo = codigo;
+        this.apellidos = apellidos;
+        this.nombre = nombre;
+        this.ci = ci;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }   
+}
---
+++ ventas/src/main/java/CapaLogica/N_Cliente.java
@@ -0,0 +1,148 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaLogica;
+
+import CapaDatos.M_Clientes;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ADRIANA
+ */
+public class N_Cliente {
+    private Conexion SQL = new Conexion();
+    private Connection cn = SQL.establecerConexion();//creo que es conectar, revisar
+    String sql="";
+    
+    
+    public DefaultTableModel mostrar(String buscar){
+        DefaultTableModel modelo;
+        
+        String[] titulos={"Codigo","Nombre","Apellidos","CI","Edad","Sexo","Telefono","Direccion"};
+        modelo = new DefaultTableModel(null,titulos);
+        String[] registro = new String [8];
+        
+        sql=("sp_buscar_cliente'"+buscar+"'");
+        
+        try{
+            Statement st=cn.createStatement();
+            ResultSet rs=st.executeQuery(sql);
+            
+            while(rs.next()){
+                registro[0]=rs.getString(1);
+                registro[1]=rs.getString(2);
+                registro[2]=rs.getString(3);
+                registro[3]=rs.getString(4);
+                registro[4]=rs.getString(5);
+                registro[5]=rs.getString(6);
+                registro[6]=rs.getString(7);
+                registro[7]=rs.getString(8);
+                registro[8]=rs.getString(9);
+                modelo.addRow(registro);
+            }
+            return modelo;
+        }catch(SQLException e){
+            JOptionPane.showConfirmDialog(null,e);
+            return null;
+        }
+    }
+        
+    public boolean insertar(M_Clientes dts){
+        sql = "{call sp_guardar_clientes (?,?,?,?,?,?,?,?)}";            
+        
+        try {
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setString(1, dts.getCodigo());
+            pst.setString(2, dts.getNombre());
+            pst.setString(3, dts.getApellidos());
+            pst.setString(4, dts.getCi());
+            pst.setInt(5, dts.getEdad());                
+            pst.setString(6, dts.getSexo());
+            pst.setString(7, dts.getTelefono());
+            pst.setString(8, dts.getDireccion());
+                
+            int n = pst.executeUpdate();
+                
+            return n != 0;
+        } catch (SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+            return false;
+        }
+    }
+        
+        public boolean editar(M_Clientes dts){
+            sql="{call sp_guardar_clientes (?,?,?,?,?,?,?,?)}";
+            
+            try{
+                PreparedStatement pst = cn.prepareStatement(sql);
+                pst.setString(1,dts.getCodigo());
+                pst.setString(2,dts.getNombre());
+                pst.setString(3,dts.getApellidos());
+                pst.setString(4,dts.getCi());
+                pst.setInt(5,dts.getEdad());
+                pst.setString(6,dts.getSexo());
+                pst.setString(7,dts.getTelefono());
+                pst.setString(8,dts.getDireccion());
+                
+                int n=pst.executeUpdate();
+                
+                return n!=0;
+            }catch(SQLException e){
+                JOptionPane.showConfirmDialog(null, e);
+                return false;
+            }
+            
+        }
+        
+        public boolean eliminar(M_Clientes dts){
+            sql= ("{call sp_eliminar_clientes (?,?,?,?,?,?,?,?)}");
+            
+            try{
+                PreparedStatement pat = cn.prepareStatement(sql);
+                pat.setString(1,dts.getCodigo());
+                pat.setString(2,dts.getNombre());
+                pat.setString(3,dts.getApellidos());
+                pat.setString(4,dts.getCi());
+                pat.setInt(5,dts.getEdad());
+                pat.setString(6,dts.getSexo());
+                pat.setString(7,dts.getTelefono());
+                pat.setString(8,dts.getDireccion());
+                
+                int n=pat.executeUpdate();
+                
+                if (n!=0)
+                    return true;
+                else
+                    return false;
+               
+            }catch(SQLException e){
+                JOptionPane.showConfirmDialog(null, e);
+                return false;
+            }
+            
+        }
+        
+        public int generarIdCliente(){
+            String sql=("selec max(idcliente) as id from clientes");
+            int cod=0;
+            
+            try{
+                Statement st=cn.createStatement();
+                ResultSet rs=st.executeQuery(sql);
+                if(rs.next()){
+                    cod=rs.getInt("id+")+1;
+                }
+            }catch(SQLException e){
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+            return cod;
+        }
+}
+
